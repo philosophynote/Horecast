641c8b1e64435dc6a1d442a2a82b39b0e906faf7
Naoki Takahashi <adverdest@gmail.com>
2021-09-18 11:57:16 +0900

    デバックファイル追加
    500エラー対策

--- Horecast/urls.py
+++ Horecast/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
 
+from forecast import views
+
+handler500 = views.my_customized_server_error
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('forecast.urls')),
