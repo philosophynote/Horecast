4b64e8ca991652957e93161cc0708b559fdd4335
Naoki Takahashi <adverdest@gmail.com>
2021-10-16 10:57:44 +0900

    10/16分

--- insert_return.py
+++ insert_return.py
@@ -3,9 +3,9 @@ import time
 from urllib.request import urlopen
 from forecast.func import umaren,umatan,sanrenpuku,sanrentan,insert_result
 
-race_id_list_a = ['2021050402{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
-race_id_list_b = ['2021090402{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
-race_id_list_c = ['2021040502{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
+race_id_list_a = ['2021050403{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
+race_id_list_b = ['2021090403{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
+race_id_list_c = ['2021040503{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
 race_id_list = race_id_list_a  + race_id_list_b + race_id_list_c
 
 insert_result(race_id_list)
