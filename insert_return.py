868599e014c4b370d671557faf9ebcc762d5d342
Naoki Takahashi <adverdest@gmail.com>
2021-12-25 09:31:47 +0900

    12/25

--- insert_return.py
+++ insert_return.py
@@ -3,11 +3,11 @@ import time
 from urllib.request import urlopen
 from forecast.func import umaren,umatan,sanrenpuku,sanrentan,insert_result
 
-race_id_list_a = ['2021060506{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
-race_id_list_b = ['2021090606{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
-race_id_list_c = ['2021070606{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
-race_id_list = race_id_list_a  + race_id_list_b + race_id_list_c
-# race_id_list = race_id_list_a  + race_id_list_b
+race_id_list_a = ['2021060507{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
+race_id_list_b = ['2021090607{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
+# race_id_list_c = ['2021070606{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
+# race_id_list = race_id_list_a  + race_id_list_b + race_id_list_c
+race_id_list = race_id_list_a  + race_id_list_b
 
 insert_result(race_id_list)
 print("結果をDBに格納しました")
