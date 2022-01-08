36ed19a537fc76d9735943d54ae0eb56b8f4daee
Naoki Takahashi <adverdest@gmail.com>
2022-01-09 08:55:08 +0900

    1/9

--- insert_race_horse.py
+++ insert_race_horse.py
@@ -1,15 +1,15 @@
 from forecast.func import insert_race_card
 from forecast.race_card import ShutubaTable as st
 
-race_id_list_a = ['2022060102{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
-race_id_list_b = ['2022070102{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
+race_id_list_a = ['2022060103{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
+race_id_list_b = ['2022070103{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
 # race_id_list_c = ['2021070606{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
 # race_id_list = race_id_list_a  + race_id_list_b + race_id_list_c
 race_id_list = race_id_list_a  + race_id_list_b 
 
 
 
-rc = st.scrape(race_id_list,"2022/01/08")
+rc = st.scrape(race_id_list,"2022/01/09")
 rc.data.reset_index(inplace=True)
 rc.data.rename(columns={"index":"race_id"},inplace=True)
 insert_race_card(rc.data)
