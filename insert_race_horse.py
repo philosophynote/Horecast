8037d53bde3fab7991995ca48d900d9eb6dcc271
philosophynote <adverdest@gmail.com>
2022-01-30 08:31:47 +0900

    0130

--- insert_race_horse.py
+++ insert_race_horse.py
@@ -1,15 +1,15 @@
 from forecast.func import insert_race_card
 from forecast.race_card import ShutubaTable as st
 
-race_id_list_a = ['2022050101{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
-race_id_list_b = ['2022070109{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
-race_id_list_c = ['2022100105{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
+race_id_list_a = ['2022050102{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
+race_id_list_b = ['2022070110{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
+race_id_list_c = ['2022100106{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
 race_id_list = race_id_list_a  + race_id_list_b + race_id_list_c
 # race_id_list = race_id_list_a  + race_id_list_b 
 
 
 
-rc = st.scrape(race_id_list,"2022/01/29")
+rc = st.scrape(race_id_list,"2022/01/30")
 rc.data.reset_index(inplace=True)
 rc.data.rename(columns={"index":"race_id"},inplace=True)
 insert_race_card(rc.data)
--- insert_return.py
+++ insert_return.py
@@ -3,9 +3,9 @@ import time
 from urllib.request import urlopen
 from forecast.func import umaren,umatan,sanrenpuku,sanrentan,insert_result
 
-race_id_list_a = ['2022050101{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
-race_id_list_b = ['2022070109{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
-race_id_list_c = ['2022100105{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
+race_id_list_a = ['2022050102{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
+race_id_list_b = ['2022070110{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
+race_id_list_c = ['2022100106{}'.format(str(i).zfill(2)) for i in range(1, 13, 1)]
 race_id_list = race_id_list_a  + race_id_list_b + race_id_list_c
 # race_id_list = race_id_list_a  + race_id_list_b
 
