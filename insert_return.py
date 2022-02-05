efdbc2449c69345c53a8eb64fdb8ff3ea1b330c3
philosophynote <adverdest@gmail.com>
2022-02-06 08:30:28 +0900

    2月6日

--- insert_return.py
+++ insert_return.py
@@ -3,11 +3,11 @@ import time
 from urllib.request import urlopen
 from forecast.func import umaren,umatan,sanrenpuku,sanrentan,insert_result
 
-race_id_list_a = ['2022050103{}'.format(
+race_id_list_a = ['2022050104{}'.format(
     str(i).zfill(2)) for i in range(1, 13, 1)]
-race_id_list_b = ['2022070111{}'.format(
+race_id_list_b = ['2022070112{}'.format(
     str(i).zfill(2)) for i in range(1, 13, 1)]
-race_id_list_c = ['2022100107{}'.format(
+race_id_list_c = ['2022100108{}'.format(
     str(i).zfill(2)) for i in range(1, 13, 1)]
 race_id_list = race_id_list_a  + race_id_list_b + race_id_list_c
 # race_id_list = race_id_list_a  + race_id_list_b
