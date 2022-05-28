59913f57438574cdee64521c779c294610f979f1
philosophynote <adverdest@gmail.com>
2022-05-29 07:11:59 +0900

    5/29

--- insert_race_horse.py
+++ insert_race_horse.py
@@ -1,18 +1,18 @@
 from forecast.func import insert_race_card
 from forecast.race_card import ShutubaTable as st
 
-race_id_list_a = ['2022050210{}'.format(
+race_id_list_a = ['2022050212{}'.format(
     str(i).zfill(2)) for i in range(1, 13, 1)]
-race_id_list_b = ['2022070306{}'.format(
+race_id_list_b = ['2022070308{}'.format(
     str(i).zfill(2)) for i in range(1, 13, 1)]
-race_id_list_c = ['2022040106{}'.format(
+race_id_list_c = ['2022040108{}'.format(
     str(i).zfill(2)) for i in range(1, 13, 1)]
 race_id_list = race_id_list_a + race_id_list_b + race_id_list_c
 # race_id_list = race_id_list_a  + race_id_list_b 
 
 
 
-rc = st.scrape(race_id_list,"2022/05/22")
+rc = st.scrape(race_id_list,"2022/05/29")
 rc.data.reset_index(inplace=True)
 rc.data.rename(columns={"index":"race_id"},inplace=True)
 insert_race_card(rc.data)
--- insert_return.py
+++ insert_return.py
@@ -3,11 +3,11 @@ import time
 from urllib.request import urlopen
 from forecast.func import umaren,umatan,sanrenpuku,sanrentan,insert_result
 
-race_id_list_a = ['2022050210{}'.format(
+race_id_list_a = ['2022050212{}'.format(
     str(i).zfill(2)) for i in range(1, 13, 1)]
-race_id_list_b = ['2022070306{}'.format(
+race_id_list_b = ['2022070308{}'.format(
     str(i).zfill(2)) for i in range(1, 13, 1)]
-race_id_list_c = ['2022040106{}'.format(
+race_id_list_c = ['2022040108{}'.format(
     str(i).zfill(2)) for i in range(1, 13, 1)]
 race_id_list = race_id_list_a + race_id_list_b + race_id_list_c
 
